69958a1745c95c3c89164a1ae39892553f4076c4
C-Iannotti <conneriannotti@outlook.com>
2023-12-29 17:10:57 -0500

    added a navbar and a table

---
+++ app/login/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+
+export default function Login() {
+  let [helloWorld, setHelloWorld] = useState<string>("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/helloworld", { cache: 'no-store' })
+      .then(async (res : Response) => {
+        return await res.json();
+      })
+      .then((data : exampleData) => {
+        setHelloWorld(data.hello);
+      });
+  }, [])
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>{helloWorld}</p>
+    </main>
+  )
+}
